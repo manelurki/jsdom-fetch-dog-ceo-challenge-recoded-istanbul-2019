d63e9088c4e6d4edfef7dfd03227205be00261a7
manel turki <menelturki@gmail.com>
2020-02-04 06:51:04 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let drop= document.getElementById('breed-dropdown');
+console.log('dog');
+
+function fetchImg() {
+  
+  return fetch (imgUrl).then(resp=> resp.json()).then(json=>renderImg(packagejson));}
+  
+function renderImg(json){
+  const main=document.getElementById('dog-image-container');
+  console.log(json.message);
+  const arr=json.message;
+  for (image of arr) {
+    const imgn= document.createElement('img');
+    imgn.src=image;
+    main.appendChild(imgn);
+  }
+}
